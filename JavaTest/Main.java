5186477c61616db834ecdcf56886b41771ae5e99
cviergutz <43346568+cviergutz@users.noreply.github.com>
2024-07-01 20:16:36 +0000

    Added Point Class

--- JavaTest/Main.java
+++ JavaTest/Main.java
@@ -1,5 +1,20 @@
+class Point {
+    private int x,y;
+
+    Point(int a, int b) {
+        x=a; y=b;
+    }
+
+    public String getString() {
+        return "("+String.valueOf(x)+","+String.valueOf(y)+")";
+    }
+}
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Point p = new Point(3, 4);
+        System.out.println(p.getString());
     }
 }
