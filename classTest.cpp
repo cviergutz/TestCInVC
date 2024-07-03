176ba1449c34afde80b7645db7d32a833563272b
cviergutz <43346568+cviergutz@users.noreply.github.com>
2024-07-03 21:11:00 +0000

    n/a

--- classTest.cpp
+++ classTest.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
+class Vehicle
+{
+protected:
+    int nrOfWheels;
 
-    for (size_t i = 0; i < argc; i++)
+public:
+    Vehicle();
+    Vehicle(int wheels);
+    ~Vehicle();
+};
+
+Vehicle::Vehicle()
+{
+}
+
+Vehicle::Vehicle(int wheels) : nrOfWheels(wheels)
+{
+}
+
+Vehicle::~Vehicle()
+{
+}
+
+class Car : public Vehicle
+{
+protected:
+    int horsePower;
+
+public:
+    Car();
+    Car(int wheels);
+    ~Car();
+};
+
+Car::Car()
+{
+}
+
+Car::Car(int wheels)
+{
+    nrOfWheels = wheels;
+}
+
+Car::~Car()
+{
+}
+
+int main(int argc, char *argv[])
+{
+    for (size_t i = 1; i < argc; i++)
     {
         cout << argv[i] << endl;
     }
-    
+
+    Vehicle *fastCar = new Car(4);
 }
